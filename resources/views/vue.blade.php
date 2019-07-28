b4c2f1c5e46664d82b91ea1a30198955169b3a17
Caesar Ali L <caesaralilamondo@gmail.com>
2019-07-28 18:48:17 +0800

    .

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="env-level" content="{{ env('CALEG_LEVEL', 'dpr') }}">
+    <meta name="env-level" content="dprd">
     <title>{{ config('app.name') }} - {{ config('app.desc') }}</title>
 
     <script src="{{ asset('js/app.js') }}" defer></script>
